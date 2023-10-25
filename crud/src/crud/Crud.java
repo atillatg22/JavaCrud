c1229345169b118a77924b6308aa2e7d5f73c553
ÁTILLA TAYLLER GAMA DA ROCHA <arocha@labcetec.net>
2023-10-25 08:30:33 -0300

    JavaCrud

---
+++ crud/src/crud/Crud.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crud;
+
+import telas.telaLista;
+
+/**
+ *
+ * @author arocha
+ */
+public class Crud {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        telaLista tela = new telaLista();
+        tela.setVisible(true);
+    }
+    
+}
